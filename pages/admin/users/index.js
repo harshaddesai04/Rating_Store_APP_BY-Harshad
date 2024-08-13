2be8d6ec5f70826ed2dad2abee74430b86271182
Rajath Rao <raorajath1998@gmail.com>
2024-08-13 08:07:21 +0530

    fixed some ui elements

--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -264,7 +264,6 @@ const Users = () => {
                     <Input
                       label="Address"
                       name={"signup-address"}
-                      className={`w-full px-3 py-2 border  border-gray-300  rounded-md shadow-sm placeholder-gray-400 sm:text-sm focus: outline-none h-[120px] resize-none`}
                       id={"signup-address"}
                       value={formData.address}
                       onChange={(e) => changeHandler(e, "address")}
